6d33221eebb3ef4d144c9c84fb0b2f6eca335340
cristian2306 <cristian.castellanos@mail.escuelaing.edu.co>
2022-10-01 18:39:27 -0500

    Proyecto react creado

---
+++ src/components/Login.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '../styles/Login.css';
+
+
+class Login extends React.Component {
+  render() {
+    return (
+      <div id="LoginDiv">
+        <form>
+            
+        </form>
+      </div>
+    );
+  }
+}
